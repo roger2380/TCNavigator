8bfeb91788d4ceaabbfe88ecdaea2c146dfaa6b2
xibiao wu <roger2380@163.com>
2017-02-14 10:20:28 +0800

    Initial Commit

---
+++ TCNavigator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TCNavigator
+//
+//  Created by xbwu on 17/2/14.
+//  Copyright © 2017年 xbwu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+@end
